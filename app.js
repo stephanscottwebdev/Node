2b12af7d4e3cccd96f509f7ecaf79c229ef13958
Stephan <stephanscottwebdev@gmail.com>
2019-01-25 15:22:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var options = {
         reconnect: true
     },
     identity: {
-        username: "MelosBouncerBot",
+        username: "BouncerBot",
         password: "oauth:up4butsn2ct5as1voont0xp4yakh16"
         },
-    channels: ["meloman1ac"]
+    channels: ["TwitchAntic"]
     
 };
 
@@ -21,13 +21,13 @@ client.connect();
 
 client.on('chat', function(channel, user, message, self) {
     if(message === "!twitter") {
-        client.action("meloman1ac", "https://twitter.com/MeloStream");
+        client.action("twitchantic", "https://twitter.com/TwitchAntic");
     }
     if(message === "!discord") {
-        client.action("meloman1ac", "https://discord.gg/ZB6Az");
+        client.action("twitchantic", "https://discord.gg/ZB6Az");
     }
 });
 //display-name=TWITCH_UserNaME
 client.on("ban", function(channel, username, reason) {
-    client.action("meloman1ac", username + " has been banned for being a dick.")
+    client.action("TwitchAntic", username + " has been banned for being a jerky sauce face.")
 });
